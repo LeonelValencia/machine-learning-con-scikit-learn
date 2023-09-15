a1b95acaa6d855446cf4b12cd946e3855902234f
Leonel Valencia <valenciaestudillo@gmail.com>
2023-09-14 18:09:09 -0600

    elasticnet una tecnica intermedia

--- regularization.py
+++ regularization.py
@@ -1,11 +1,14 @@
 # Importamos las bibliotecas
 import pandas as pd
 import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Importamos los modelos de sklearn
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import Lasso
 from sklearn.linear_model import Ridge
+from sklearn.linear_model import ElasticNet
 
 # Importamos las metricas de entrenamiento y el error medio cuadrado
 from sklearn.model_selection import train_test_split
@@ -76,3 +79,23 @@ if __name__ == "__main__":
     print("Coef RIDGE")
     print(modelRidge.coef_)
     
+    def modelElastic(alpha=1):
+        modelElastic= ElasticNet(random_state=0, alpha=alpha)
+        modelElastic.fit(X_train, y_train)
+        y_predic_elastic=modelElastic.predict(X_test)
+        # loss function
+        elastic_loss = mean_squared_error(y_test, y_predic_elastic)
+        return elastic_loss
+
+    alphas = np.arange(0,1,0.01)
+    loss_total = []
+    for i in alphas:
+        res = modelElastic(i)
+        loss_total.append(res)
+
+    loss_total = np.array(loss_total)
+    plt.plot(alphas, loss_total)
+    plt.xlabel('alphas')
+    plt.ylabel('Loss Elastic')
+    plt.text(0.02, 0.8, 'loss min:{}'.format(np.min(loss_total)), fontsize=7)
+    plt.show()
